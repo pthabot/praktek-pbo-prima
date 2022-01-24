d2163dbb308badcdeba3471707121021817c3ce5
pthabot <prima.fams@gmail.com>
2022-01-24 10:00:21 +0700

    tugas pbo

--- angkaGanjilGenap.js
+++ angkaGanjilGenap.js
@@ -9,3 +9,15 @@ for (let i = 1; i <= 100; i += 3) {
 }
 
 console.log('ABIS')
+
+for (let i = 50; i <= 100; i += 5) {
+if (i % 5 == 3) {
+        console.log(`${i} - habis `);
+    }
+}
+
+for (let i = 50; i <= 100; i += 5) {
+    if (i % 5 == 3) {
+            console.log(`${i} - habis `);
+        }
+    }
---
+++ bintang.js
@@ -0,0 +1,6 @@
+for (let i=0; i<5; i++) {
+    console.log("Loop 1", i);
+    for (let j=0; j<4 ; j++) {
+        console.log("Loop 2", j);
+    }
+}
